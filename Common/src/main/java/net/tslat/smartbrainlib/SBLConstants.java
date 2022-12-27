36c18d4533f46036f9f187a04ab87e99a36eafb2
Kli Kli <klikli@outlook.at>
2022-12-27 09:07:15 +0100

    chore: prepare for upstream PR

--- Common/src/main/java/net/tslat/smartbrainlib/SBLConstants.java
+++ Common/src/main/java/net/tslat/smartbrainlib/SBLConstants.java
@@ -6,8 +6,8 @@ import org.apache.logging.log4j.Logger;
 import java.util.ServiceLoader;
 
 public class SBLConstants {
-	public static final String MOD_ID = "forgebrainlib";
-	public static final String MOD_NAME = "ForgeBrainLib";
+	public static final String MOD_ID = "smartbrainlib";
+	public static final String MOD_NAME = "SmartBrainLib";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 	public static final SBLLoader SBL_LOADER = ServiceLoader.load(SBLLoader.class).findFirst().get();
 }
